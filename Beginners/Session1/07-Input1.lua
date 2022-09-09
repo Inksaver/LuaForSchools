506bc4e19ab80caaabeeb58d5b0b346ff60e3c52
Inksaver <Inksaver@users.noreply.github.com>
2022-09-09 20:41:28 +0100

    Update 07-Input1.lua

--- Beginners/Session1/07-Input1.lua
+++ Beginners/Session1/07-Input1.lua
@@ -9,11 +9,15 @@ function main()
 	
 	io.write("Type your name_") 	-- io.write() does NOT move to the next line
 	name = io.read()				-- io.read() stores what you type when you press Enter
+	io.write("Type your age_")
+	age = io.read()
 	
-	--[[if you typed in the name "Fred", the variable called 'name'
-		now contains the letters 'F', 'r', 'e' and 'd' : a STRING
+	--[[You can see we are using the 2 lines
+		io.write(Some text here)
+		var = io.read() -- var is any variable such as name, age, height etc
 	]]
-	print("Hello "..name)			-- the .. dots join words (strings) together
+	print("Hello "..name)
+	print("You are ".. age .. " years old")			-- the .. dots join words (strings) together
 end
 
 main()
