036ffeff08eb48f901593fee4840a3dad340f755
Inksaver <Inksaver@users.noreply.github.com>
2022-09-12 13:58:24 +0100

    Update 06-Assignment 1.lua

--- Beginners/Section1/06-Assignment 1.lua
+++ Beginners/Section1/06-Assignment 1.lua
@@ -1,22 +1,29 @@
--- Github defaults to 1 tab = 8 spaces so layout looks a bit weird. Use 4 spaces in your IDE/Text editor
---	Use the template below to create an output that looks like this:
---	Ignore the --[[ and ]]. They just allow multi-line comments!
+--[[
+https://www.w3schools.com/charsets/ref_utf_box.asp
+utf8 box characters stored here for easy copy/paste:
+
+┌ ┬ ┐ ─ ╔ ╦ ╗ ═
+ 
+├ ┼ ┤ │ ╠ ╬ ╣ ║
+ 
+└ ┴ ┘   ╚ ╩ ╝
+Use the template below to create an output that looks like one of these:
+Ignore the [ and ]. They just allow multi-line comments!
+]]
 
 --[[
-
-=========================================
-|                                       |
-|    Welcome to Lua programming!        |
-|        This line is tabbed in x2      |
-|        So is this one!                |
-+++++++++++++++++++++++++++++++++++++++++
-	
+                                           Pre Windows 10 (1909) will not display UTF8
+╔═══════════════════════════════════════╗  =========================================
+║                                       ║  |                                       |
+║    Welcome to Lua programming!        ║  |    Welcome to Lua programming!        |
+║        This line is tabbed in x2      ║  |        This line is tabbed in x2      |
+║        So is this one!                ║  |        So is this one!                |
+╚═══════════════════════════════════════╝  +++++++++++++++++++++++++++++++++++++++++
 ]]
 	
 -- Use either print() or io.write()
 -- (remember io.write() does NOT move to a new line
--- Also use \t to line up the text
--- The comment above is 10 TAB spaces wide...
+-- Also use \t to line up the text (or spaces)
 
 function main()
 	-- your code goes here
