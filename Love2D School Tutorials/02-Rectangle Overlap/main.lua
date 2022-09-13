d1a072e8112b3241fa24c21c350981f4a595170d
Inksaver <Inksaver@users.noreply.github.com>
2022-09-13 15:28:43 +0100

    Update main.lua

--- Love2D School Tutorials/02-Rectangle Overlap/main.lua
+++ Love2D School Tutorials/02-Rectangle Overlap/main.lua
@@ -1,14 +1,14 @@
 function love.load()
-	-- set variables for 2 rectangles
-	x1 = 100 
-	y1 = 100
-	w1 = 100
-	h1 = 100
-	
-	x2 = 150
-	y2 = 150
-	w2 = 100
-	h2 = 100
+    -- set variables for 2 rectangles
+    x1 = 100 
+    y1 = 100
+    w1 = 100
+    h1 = 100
+
+    x2 = 150
+    y2 = 150
+    w2 = 100
+    h2 = 100
 end
 
 function love.update(dt)
@@ -16,8 +16,9 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.setColor(1, 0, 0, 1) -- set colour to red
-	love.graphics.rectangle("fill", x1, y1, w1, h1)
-	love.graphics.setColor(0, 1, 0, 0.7) -- set colour to green, transparency to 50%
-	love.graphics.rectangle("fill", x2, y2, w2, h2)
+    love.graphics.setBackgroundColor(1, 1, 1, 1)
+    love.graphics.setColor(1, 0, 0, 1) -- set colour to red
+    love.graphics.rectangle("fill", x1, y1, w1, h1)
+    love.graphics.setColor(0, 1, 0, 0.7) -- set colour to green, transparency to 50%
+    love.graphics.rectangle("fill", x2, y2, w2, h2)
 end
