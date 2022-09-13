16ef5a34a39bd914b2ea8ef183fab58052e00919
Inksaver <Inksaver@users.noreply.github.com>
2022-09-13 16:12:08 +0100

    Update main.lua

--- Love2D School Tutorials/05-Lines/main.lua
+++ Love2D School Tutorials/05-Lines/main.lua
@@ -1,9 +1,9 @@
 function love.load()
-	colour = love.graphics.setColor
-	line = love.graphics.line
-	red = {1,0,0,1}
-	green = {0,1,0,1}
-	blue = {0,0,1,1}
+    colour = love.graphics.setColor -- use a variable to store a function
+    line = love.graphics.line
+    red = {1,0,0,1}
+    green = {0,1,0,1}
+    blue = {0,0,1,1}
 end
 
 function love.update(dt)
@@ -11,14 +11,15 @@ function love.update(dt)
 end
 
 function love.draw()
-	-- love.graphics.line( x1, y1, x2, y2, ... ) You can continue passing point positions to draw a polyline.
-	colour(green)
-	love.graphics.line(0, 20, 800, 20)
-	colour(red)
-	line(200,50, 400,50, 500, 250, 100,250, 200,50)   -- last pair is a repeat to complete the trapezoid
+    love.graphics.setBackgroundColor(1, 1, 1, 1)
+    -- love.graphics.line( x1, y1, x2, y2, ... ) You can continue passing point positions to draw a polyline.
+    colour(green)
+    love.graphics.line(0, 20, 800, 20)
+    colour(red)
+    line(200,50, 400,50, 500, 250, 100,250, 200,50)   -- last pair is a repeat to complete the trapezoid
 	
-	local mx, my = love.mouse.getPosition()
-	local windowWidth, windowHeight = love.graphics.getDimensions()
-	colour(blue)
-	love.graphics.line(windowWidth/2, windowHeight/2, mx, my)
+    local mx, my = love.mouse.getPosition()
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    colour(blue)
+    love.graphics.line(windowWidth/2, windowHeight/2, mx, my)
 end
