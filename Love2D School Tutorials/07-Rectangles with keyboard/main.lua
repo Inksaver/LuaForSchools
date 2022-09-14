0a9a246077413982f3959de187fa7f9f8820dc64
Inksaver <Inksaver@users.noreply.github.com>
2022-09-14 14:31:15 +0100

    Update main.lua

--- Love2D School Tutorials/07-Rectangles with keyboard/main.lua
+++ Love2D School Tutorials/07-Rectangles with keyboard/main.lua
@@ -1,25 +1,36 @@
 function love.load()
-	x = 0 		-- starting position of rectangle horizontal
-	y = 0		-- starting position of rectangle vertical
-	speed = 10	-- how fast the rectangle should travel
+    rect = {x = 0, y = 0, w = 100, h = 100, speed = 10}
 end
 
 function love.update(dt)
-	if love.keyboard.isDown('w') and y > 0 then
-		y = y - speed
-	end
-	if love.keyboard.isDown('a') and x > 0 then
-		x = x - speed
-	end
-	if love.keyboard.isDown('s') and y < 600 then
-		y = y + speed
-	end
-	if love.keyboard.isDown('d') and x < 800 then
-		x = x + speed
-	end
+    if love.keyboard.isDown('w') then
+        rect.y = rect.y - rect.speed
+    end
+    if love.keyboard.isDown('a') then
+        rect.x = rect.x - rect.speed
+    end
+    if love.keyboard.isDown('s') then
+        rect.y = rect.y + rect.speed
+    end
+    if love.keyboard.isDown('d') then
+        rect.x = rect.x + rect.speed
+    end
+    if rect.x > 800 - rect.w then
+        rect.x = 800 - rect.w
+    end
+    if rect.x < 0 then
+        rect.x = 0
+    end
+    if rect.y > 600 - rect.h then
+        rect.y = 600 - rect.h
+    end
+    if rect.y < 0 then
+        rect.y = 0
+    end
 end
 
 function love.draw()
-	love.graphics.setColor(1, 0, 0) -- set colour to red
-	love.graphics.rectangle("fill", x, y, 100, 100)
+    love.graphics.setBackgroundColor(1, 1, 1, 1)
+    love.graphics.setColor(1, 0, 0) -- set colour to red
+    love.graphics.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
 end
