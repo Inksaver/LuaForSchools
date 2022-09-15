1c5fb238f16d62ccdec0337909be632efded37f3
Inksaver <Inksaver@users.noreply.github.com>
2022-09-15 07:47:57 +0100

    Update main.lua

--- Love2D School Tutorials/08-Rectangles with keyboard-v2/main.lua
+++ Love2D School Tutorials/08-Rectangles with keyboard-v2/main.lua
@@ -1,30 +1,38 @@
 local WIDTH = love.graphics.getWidth()
 local HEIGHT = love.graphics.getHeight()
 function love.load()
-	rect1 = {}
-	rect1.x = 0 
-	rect1.y = 0
-	rect1.w = 100
-	rect1.h = 100
-	rect1.speed = 10
+    -- rect table declared in a more compact form
+    rect1 = 
+    {
+        x = 0, 
+        y = 0,
+        w = 100,
+        h = 100,
+        speed = 10
+    }
+    -- assign functions to variables
+    key = love.keyboard.isDown 
+    colour = love.graphics.setColor
+    rectangle = love.graphics.rectangle
 end
 
 function love.update(dt)
-	if (love.keyboard.isDown('w') or love.keyboard.isDown('up')) and rect1.y > 0 then
-		rect1.y = rect1.y - rect1.speed
-	end
-	if (love.keyboard.isDown('a')  or love.keyboard.isDown('left')) and rect1.x > 0 then
-		rect1.x = rect1.x - rect1.speed
-	end
-	if (love.keyboard.isDown('s') or love.keyboard.isDown('down')) and rect1.y < HEIGHT - rect1.h then
-		rect1.y = rect1.y + rect1.speed
-	end
-	if (love.keyboard.isDown('d') or love.keyboard.isDown('right')) and rect1.x < WIDTH - rect1.w then
-		rect1.x = rect1.x + rect1.speed
-	end
+    -- combining selections to check for key press and ability to move
+    if (key('w') or key('up')) and rect1.y > 0 then
+        rect1.y = rect1.y - rect1.speed
+    end
+    if (key('a')  or key('left')) and rect1.x > 0 then
+        rect1.x = rect1.x - rect1.speed
+    end
+    if (key('s') or key('down')) and rect1.y < HEIGHT - rect1.h then
+        rect1.y = rect1.y + rect1.speed
+    end
+    if (key('d') or key('right')) and rect1.x < WIDTH - rect1.w then
+        rect1.x = rect1.x + rect1.speed
+    end
 end
 
 function love.draw()
-	love.graphics.setColor(1, 0, 0, 1) -- set colour to red
-	love.graphics.rectangle("fill", rect1.x, rect1.y, rect1.w, rect1.h)
+    colour(1, 0, 0, 1) -- set colour to red
+    rectangle("fill", rect1.x, rect1.y, rect1.w, rect1.h)
 end
