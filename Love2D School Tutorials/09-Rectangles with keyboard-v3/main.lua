ce98c913b7f4085aeb2906c1de0dc64d74b371f2
Inksaver <Inksaver@users.noreply.github.com>
2022-09-15 13:44:14 +0100

    Update main.lua

--- Love2D School Tutorials/09-Rectangles with keyboard-v3/main.lua
+++ Love2D School Tutorials/09-Rectangles with keyboard-v3/main.lua
@@ -1,34 +1,43 @@
 local WIDTH = love.graphics.getWidth()
 local HEIGHT = love.graphics.getHeight()
 function love.load()
-	rect1 = {}
-	rect1.x = 0 
-	rect1.y = 0
-	rect1.w = 100
-	rect1.h = 100
-	rect1.speed = 10
+    rect1 = 
+    {
+        x = 0, 
+        y = 0,
+        w = 100,
+        h = 100,
+        speed = 10
+    }
+	key       = love.keyboard.isDown 
+    colour    = love.graphics.setColor
+    rectangle = love.graphics.rectangle
+    red       = {1, 0, 0}
+    green     = {0, 1, 0}
+    blue      = {0, 0, 1}
+    yellow    = {1, 1, 0}
 end
 
 function love.update(dt)
-	if (love.keyboard.isDown('w') or love.keyboard.isDown('up')) and rect1.y > 0 then
-		rect1.y = rect1.y - rect1.speed
-		love.graphics.setColor(0, 0, 1) -- set colour to blue
-	end
-	if (love.keyboard.isDown('a')  or love.keyboard.isDown('left')) and rect1.x > 0 then
-		rect1.x = rect1.x - rect1.speed
-		love.graphics.setColor(1, 0, 0) -- set colour to red
-	end
-	if (love.keyboard.isDown('s') or love.keyboard.isDown('down')) and rect1.y < HEIGHT - rect1.h then
-		rect1.y = rect1.y + rect1.speed
-		love.graphics.setColor(0, 1, 0) -- set colour to green
-	end
-	if (love.keyboard.isDown('d') or love.keyboard.isDown('right')) and rect1.x < WIDTH - rect1.w then
-		rect1.x = rect1.x + rect1.speed
-		love.graphics.setColor(1, 1, 0) -- set colour to yellow
-	end
+	-- combining selections to check for key press and ability to move
+    if (key('w') or key('up')) and rect1.y > 0 then
+        rect1.y = rect1.y - rect1.speed
+		colour(blue) -- set colour to blue
+    end
+    if (key('a')  or key('left')) and rect1.x > 0 then
+        rect1.x = rect1.x - rect1.speed
+		colour(red) -- set colour to red
+    end
+    if (key('s') or key('down')) and rect1.y < HEIGHT - rect1.h then
+        rect1.y = rect1.y + rect1.speed
+		colour(green) -- set colour to green
+    end
+    if (key('d') or key('right')) and rect1.x < WIDTH - rect1.w then
+        rect1.x = rect1.x + rect1.speed
+		colour(yellow) -- set colour to yellow
+    end
 end
 
 function love.draw()
-	--love.graphics.setColor(1, 0, 0, 1) -- set colour to red
-	love.graphics.rectangle("fill", rect1.x, rect1.y, rect1.w, rect1.h)
+    rectangle("fill", rect1.x, rect1.y, rect1.w, rect1.h)
 end
